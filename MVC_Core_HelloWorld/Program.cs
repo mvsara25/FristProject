45c1db83d1032153cf084f3525390337de7f1b21
saravanan m <msaravanan1986@gmail.com>
2024-04-29 07:25:22 +0530

    first commit

---
+++ MVC_Core_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Enabling MVC
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+//Configuring Routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
